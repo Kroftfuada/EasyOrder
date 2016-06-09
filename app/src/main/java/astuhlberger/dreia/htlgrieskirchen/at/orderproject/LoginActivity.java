8fe074c5a3aec93a7f92accfbcc3bb0ae390081b
andi1479 <andreas.stuhlberger@web.com>
2016-06-09 15:53:21 +0200

    Final Push 09.06.16 - missing changes <- commit before + pw encryption

--- app/src/main/java/astuhlberger/dreia/htlgrieskirchen/at/orderproject/LoginActivity.java
+++ app/src/main/java/astuhlberger/dreia/htlgrieskirchen/at/orderproject/LoginActivity.java
@@ -1,10 +1,16 @@
 package astuhlberger.dreia.htlgrieskirchen.at.orderproject;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by nprechtl on 09.06.2016.
@@ -13,19 +19,38 @@ public class LoginActivity extends Activity{
 
     EditText username, password;
     String login_username,login_password;
-
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         username = (EditText)findViewById(R.id.input_username);
         password = (EditText)findViewById(R.id.input_password);
+        login = (Button)findViewById(R.id.btn_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login_username = username.getText().toString();
+                login_password = password.getText().toString();
+                Log.d("Firebase",login_username);
+
+                if(login_username != null && login_password != null)
+                {
+                    Intent returnIntent = new Intent();
+                    returnIntent.putExtra("username",login_username);
+                    returnIntent.putExtra("password",login_password);
+                    setResult(Activity.RESULT_OK,returnIntent);
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Passwort oder Username falsch",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
-    public void login()
-    {
-        login_username = username.getText().toString();
-        login_password = password.getText().toString();
 
 
-    }
+
 }
--- app/src/main/java/astuhlberger/dreia/htlgrieskirchen/at/orderproject/MainActivity.java
+++ app/src/main/java/astuhlberger/dreia/htlgrieskirchen/at/orderproject/MainActivity.java
@@ -1,37 +1,65 @@
 package astuhlberger.dreia.htlgrieskirchen.at.orderproject;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-
-     //Commit by astuhlberger
-     //Commit by famayr
-     //Commit by nprechtl
-     //Commit by DJ Darius Duck
-
-
-
-
+    Firebase dataBase;
+    DataSnapshot snapshot;
+    String username, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+        dataBase = new Firebase("https://easyorder.firebaseIO.com");
+
+
 
         //Aufruf der LoginActivity
         Intent i = new Intent(this,LoginActivity.class);
         startActivityForResult(i,1);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 1)
+        {
+            username = data.getStringExtra("username");
+            password = data.getStringExtra("password");
+            Log.d("Firebase",username + password);
 
+            dataBase.addValueEventListener(new ValueEventListener() {
+                public void onDataChange(DataSnapshot snapshot) {
 
+                    if(snapshot.child("id").child("username").getValue().toString().contains(username) && snapshot.child("id").child("password").getValue().toString().contains(password))
+                    {
+                        Toast.makeText(getApplicationContext(),"LOGIN COMPLETE",Toast.LENGTH_LONG).show();;
+                    }
+                }
 
+                public void onCancelled(FirebaseError firebaseError) {
+                    System.out.println("The read failed: " + firebaseError.getMessage());
+                }
+            });
+
+        }
     }
 
     @Override
