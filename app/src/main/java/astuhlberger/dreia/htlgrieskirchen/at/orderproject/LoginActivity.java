e69d532a94c830b5933e1c21940ad20f4a173282
andi1479 <andreas.stuhlberger@web.com>
2016-06-09 14:12:47 +0200

    Login - no logo - no databaseconnection - new account not implemented

--- app/src/main/java/astuhlberger/dreia/htlgrieskirchen/at/orderproject/LoginActivity.java
+++ app/src/main/java/astuhlberger/dreia/htlgrieskirchen/at/orderproject/LoginActivity.java
@@ -2,15 +2,29 @@ package astuhlberger.dreia.htlgrieskirchen.at.orderproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 /**
  * Created by nprechtl on 09.06.2016.
  */
 public class LoginActivity extends Activity{
 
+    EditText username, password;
+    String login_username,login_password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        username = (EditText)findViewById(R.id.input_username);
+        password = (EditText)findViewById(R.id.input_password);
+
+
+    }
+    public void login()
+    {
+        login_username = username.getText().toString();
+        login_password = password.getText().toString();
+
+
     }
 }
--- app/src/main/java/astuhlberger/dreia/htlgrieskirchen/at/orderproject/MainActivity.java
+++ app/src/main/java/astuhlberger/dreia/htlgrieskirchen/at/orderproject/MainActivity.java
@@ -1,5 +1,6 @@
 package astuhlberger.dreia.htlgrieskirchen.at.orderproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -23,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Aufruf der LoginActivity
+        Intent i = new Intent(this,LoginActivity.class);
+        startActivityForResult(i,1);
+
+
+
+
     }
 
     @Override
