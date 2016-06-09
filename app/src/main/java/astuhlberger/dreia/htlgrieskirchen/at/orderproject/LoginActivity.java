9a41e969d4d31fe6ba7b782f7ffb41281540314a
nprechtl <nico.prechtl@gmx.at>
2016-06-09 13:57:10 +0200

    LoginDesign

---
+++ app/src/main/java/astuhlberger/dreia/htlgrieskirchen/at/orderproject/LoginActivity.java
@@ -0,0 +1,16 @@
+package astuhlberger.dreia.htlgrieskirchen.at.orderproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by nprechtl on 09.06.2016.
+ */
+public class LoginActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
