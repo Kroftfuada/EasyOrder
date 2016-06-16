1d738959a4f670a5124e47eef0c5c583c82a5118
Darius <kelemendarius@yahoo.com>
2016-06-16 04:37:41 -0700

    add_restaurant layout

--- app/src/main/java/astuhlberger/dreia/htlgrieskirchen/at/orderproject/AddRestaurantActivity.java
+++ app/src/main/java/astuhlberger/dreia/htlgrieskirchen/at/orderproject/AddRestaurantActivity.java
@@ -1,57 +1,10 @@
 package astuhlberger.dreia.htlgrieskirchen.at.orderproject;
 
 import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 /**
  * Created by nprechtl on 16.06.2016.
  */
-public class AddRestaurantActivity extends Activity
+public class AddRestaurantActivity extends Activity {
 
-{
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_restaurant);
-
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.action_bills: showBills();
-                break;
-            case R.id.action_groups: showGroups();
-                break;
-            case R.id.action_orders: showOrders();
-                break;
-            case R.id.action_logout: logout();
-                break;
-
-        }
-        return super.onOptionsItemSelected(item);
-
-    }
-
-    private void logout() {
-    }
-
-    private void showOrders() {
-    }
-
-    private void showGroups() {
-    }
-
-    private void showBills() {
-    }
 }
