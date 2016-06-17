11c25578c7155e41ac08d6a0ef13e22f32e6d7b6
Darius <kelemendarius@yahoo.com>
2016-06-17 04:15:59 -0700

    ProductActivity

--- app/src/main/java/astuhlberger/dreia/htlgrieskirchen/at/orderproject/ProductActivity.java
+++ app/src/main/java/astuhlberger/dreia/htlgrieskirchen/at/orderproject/ProductActivity.java
@@ -1,15 +1,22 @@
 package astuhlberger.dreia.htlgrieskirchen.at.orderproject;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.NumberPicker;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Marcus on 16.06.2016.
@@ -18,13 +25,46 @@ public class ProductActivity extends Activity {
     ListView billList;
     Context context;
     View view;
+    HashMap<String, Integer> product;
     ArrayList<String> items = new ArrayList<String>();
+
+    //TODO: Layout für Dialog und Activity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        displayItems();
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.product_activity);
+
+
+        displayItems();
+
+
+        billList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                showDialogs(items.get(position));
+            }
+        });
     }
+
+    private void showDialogs(final String s) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LinearLayout vDialog = (LinearLayout) getLayoutInflater().inflate(R.layout.product_selection, null);
+        builder.setView(vDialog);
+        builder.setTitle(s + ":");
+        final NumberPicker numberpicker = (NumberPicker) vDialog.findViewById(R.id.numberPicker);
+        builder.setPositiveButton("Select", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                int number = numberpicker.getValue();
+                product.put(s, number);
+            }
+        });
+
+        builder.setNegativeButton("Cancel", null);
+        builder.show();
+    }
+
 
     private void displayItems(){
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.activity_list_item, items);
